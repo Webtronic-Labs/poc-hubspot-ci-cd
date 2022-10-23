284ca470cae3cfd35d31b373951c7e4f26333a2c
none <none>
2022-10-23 20:30:50 +0200

    Testing integration

--- modules/react-component-demo.module/src/App.js
+++ modules/react-component-demo.module/src/App.js
@@ -2,17 +2,14 @@ import React from 'react';
 import './App.scss';
 
 function App({ moduleData }) {
-  // eslint-disable-next-line no-console
   console.log(
     'all of your data typically accessed via the "module" keyword in HubL is available as JSON here!',
     moduleData,
   );
+
   return (
-    <div className="cms-react-boilerplate__container">
-      <div className="spinning-logo__container"></div>
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
+    <div className="header-main">
+      <p>This code is using react.</p>
     </div>
   );
 }
